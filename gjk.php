4a77b37da5e94312f472bd577514063648ecc4b1
kvnmtnckz27 <54585137+kvnmtnckz27@users.noreply.github.com>
2020-05-11 23:36:50 +0800

    Update gjk.php

--- gjk.php
+++ gjk.php
@@ -167,7 +167,7 @@ echo color("green","     ===========================\n");
                                         $debug['text'] = $pesan;
                                         $debug['respon'] = json_decode($datas, true);
         
-         setpin: echo "\n".color("nevy","?] Mau set pin?: y/n "); $pilih1 = trim(fgets(STDIN)); if($pilih1 == "y" || $pilih1 == "Y"){ //if($pilih1 == "y" && strpos($no, "628")){ echo color("red","========( PIN ANDA = 112233 )========")."\n"; $data2 = '{"pin":"112233"}'; $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid); echo "OTP SET PIN: "; $otpsetpin = trim(fgets(STDIN)); $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid); echo $verifotpsetpin; }else if($pilih1 == "n" || $pilih1 == "N"){ die(); }else{ echo color("red","-] GAGAL!!!\n"); } } } } }else{ goto setpin; } }else{
+         
          }
          }
         }
